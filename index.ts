6068c3a39ce098d67dcaa1d130a0eacfe62699bb
Caio Ceretta <caioceretta@gmail.com>
2022-10-02 22:45:42 -0300

    Added advantages and disadvantages

---
+++ index.ts
@@ -0,0 +1,21 @@
+/*
+ Vantas e desvantagens do SOLID
+
+  Vantagens:
+  - Código modular, cada modulo faz uma coisa especifica, ficando mais fácil saber onde está cada coisa.
+  - Fácil manutenção
+  - Código reutilizável (D.R.Y - Dont Repeat Yourself)
+  - Código testável, baixo acoplamento, uma classe não depende de outra
+  - Baixo acoplamento e alta coesão
+  - Código expansível, porque as mudanças em uma classe/interface não quebraria códigos feitos anteriormente
+  - Separation of Concerns, cada classe precisa ser coesa e fazer o que ela realmente faz, não mais do que isso, uma classe só deve ter uma razão pra mudar
+
+
+  Desvantagens:
+  - Complexidade aumenta, muito por causa da modulação
+  - Tenha cuidados com: YAGNI (You aren't gonna need it) e KISS (Keep It Simple, Stupid! (Mantenha o seu software simples))
+  - Quantidade de códigos
+  - Tempo de desenvolvimento aumenta
+
+
+ */
